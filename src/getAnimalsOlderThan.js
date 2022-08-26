26e51b32986c11080ebbb6b4ea8368371a480350
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-08-26 16:17:47 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const specie = species.find((el) => el.name === animal);
+  return specie.residents.every((el) => el.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
+
+console.log(getAnimalsOlderThan('giraffes', 10));

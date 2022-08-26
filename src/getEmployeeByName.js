a446659c7678323c7f0adc30cd304db0812dc4fb
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-08-26 16:43:21 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,18 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  try {
+    const resut = employees.find((employee) => employee.firstName === employeeName
+    || employee.lastName === employeeName);
+    if (resut === undefined) {
+      throw new Error('Informações inválidas');
+    }
+    return resut;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 module.exports = getEmployeeByName;

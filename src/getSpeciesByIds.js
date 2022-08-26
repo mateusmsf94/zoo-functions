20f7fba5a6a21236e4c441a0b670bcd7caa334fa
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-08-26 15:34:40 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,22 +2,20 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  if(ids.length === 0) {
-    return []
-  } else if (ids.length === 1) {
-    const result = []
-    result.push(species.find(specie => specie.id === ids[0]))
-    return result
-  } else {
-    const resultAr = []
-    species.forEach(specie => {
-      if(ids.includes(specie.id)){
-        resultAr.push(specie)
-      }
-    })
-    return resultAr
+  if (ids.length === 0) {
+    return [];
+  } if (ids.length === 1) {
+    const result = [];
+    result.push(species.find((specie) => specie.id === ids[0]));
+    return result;
   }
-  
+  const resultAr = [];
+  species.forEach((specie) => {
+    if (ids.includes(specie.id)) {
+      resultAr.push(specie);
+    }
+  });
+  return resultAr;
 }
 
 module.exports = getSpeciesByIds;

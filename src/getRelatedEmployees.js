c9729537a1d8bb4a1a68d0714c7fb6b2d71f17f0
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-08-26 21:01:03 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const managersAll = [stephanieId, olaId, burlId];
+
 function isManager(id) {
-  // seu código aqui
+  return (managersAll.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  try {
+    if (!isManager(managerId)) {
+      throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+    }
+    const result = employees.filter((emp) => emp.managers.includes(managerId));
+    return result.map((el) => `${el.firstName} ${el.lastName}`);
+  } catch (error) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };

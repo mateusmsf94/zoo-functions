175f49e6b87bdfa41b2fec8f0e79efc0a764f7b0
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-08-27 21:40:08 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(animal = {}) {
+  if (Object.keys(animal).length === 0) {
+    return species.reduce(
+      (acc, currSpc) =>
+        Object.assign(acc, { [currSpc.name]: currSpc.residents.length }),
+      {},
+    );
+  }
+
+  const find1 = species.find((spc) => spc.name === animal.specie);
+  if (Object.keys(animal).length === 1) {
+    return find1.residents.length;
+  }
+
+  if (Object.keys(animal).length === 2) {
+    const result = find1.residents.filter((el) => el.sex === animal.sex);
+    return result.length;
+  }
 }
 
 module.exports = countAnimals;

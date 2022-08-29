09940cb8ca4c044608d55f15fedd54eed2ebb709
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-08-29 12:53:18 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,43 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getSpecies(arr) {
+  return arr.map((el) => species.find((spc) => spc.id === el).name);
+}
+function getLocation(arr) {
+  return arr.map((el) => species.find((spc) => spc.id === el).location);
 }
 
+function getData({ id, firstName, lastName, responsibleFor }) {
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: getSpecies(responsibleFor),
+    locations: getLocation(responsibleFor),
+  };
+}
+
+function handleInput({ name, id }) {
+  let employe;
+  if (id) {
+    employe = employees.find((el) => el.id === id);
+  }
+  if (name) {
+    employe = employees.find((el) => el.firstName === name || el.lastName === name);
+  }
+  if (employe === undefined) throw new Error('Informações inválidas');
+  return employe;
+}
+
+function getEmployeesCoverage(empl) {
+  let employe;
+  if (!empl) {
+    employe = employees.map((el) => getData(el));
+    return employe;
+  }
+  employe = handleInput(empl);
+
+  return getData(employe);
+}
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,28 +1,26 @@
 const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimal (idAnimal) {
-  return species.find(el => el.id === idAnimal)
+function getAnimal(idAnimal) {
+  return species.find((el) => el.id === idAnimal);
 }
 
-const example = getAnimal('0938aa23-f153-4937-9f88-4858b24d6bce')
+// const example = getAnimal('0938aa23-f153-4937-9f88-4858b24d6bce');
 // console.log(example.residents.reduce((oldest, curr) => {
-  // return oldest.age < curr.age ? curr : oldest} ))
+// return oldest.age < curr.age ? curr : oldest} ))
 
-function getOlder ({residents}) {
-  const result = residents.reduce((oldest, curr) => {
-    return oldest.age < curr.age ? curr : oldest} )
-  return Object.values(result)
+function getOlder({ residents }) {
+  const result = residents.reduce((oldest, curr) => (oldest.age < curr.age ? curr : oldest));
+  return Object.values(result);
 }
 // console.log(getOlder(example))
 
 // console.log(getAnimal('0938aa23-f153-4937-9f88-4858b24d6bce'))
 
 function getOldestFromFirstSpecies(id) {
-  const employe = employees.find(el => el.id === id)
-  const firstSpecie = employe.responsibleFor[0]
-  return getOlder(getAnimal(firstSpecie))
+  const employe = employees.find((el) => el.id === id);
+  const firstSpecie = employe.responsibleFor[0];
+  return getOlder(getAnimal(firstSpecie));
 }
-
 
 module.exports = getOldestFromFirstSpecies;

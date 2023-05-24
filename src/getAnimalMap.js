08dca15f9f5f3412822607307013b91328552f40
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2023-05-24 12:30:31 -0300

    fix

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,92 +1,32 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-// function getAnimalinArea(area) {
-//   return species.filter((el) => el.location.includes(area))
-//     .map((el) => el.name);
-// }
+function makeAnimalMap(specie, sorted, sex) {
+  const animalsNames = specie.residents
+    .filter((animal) => animal.sex === sex || sex === 'all')
+    .map((animal) => animal.name);
 
-// const empty = (includeNames, sorted, sex) => !includeNames && !sorted && !sex;
+  if (sorted) animalsNames.sort();
+  const result = {};
+  result[specie.name] = animalsNames;
 
-// function filterHandler({ includeNames, sorted, sex }) {
-//   if (empty(includeNames, sorted, sex)) {
+  return result;
+}
 
-//   }
-// }
+function getAnimalMap(options = {}) {
+  const initObj = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [],
+  };
+  const { includeNames = false, sorted = false, sex = 'all' } = options;
+  if (!includeNames) {
+    species.forEach((spc) => initObj[spc.location].push(spc.name));
+  } else {
+    species.forEach((spc) => initObj[spc.location].push(makeAnimalMap(spc, sorted, sex)));
+  }
 
-// const defautlObj = {
-//   NE: getAnimalinArea('NE'),
-//   NW: getAnimalinArea('NW'),
-//   SE: getAnimalinArea('SE'),
-//   SW: getAnimalinArea('SW'),
-// };
-
-// arrLoc = Object.keys(defautlObj);
-
-// function getAllI(arr) {
-//   arr.map((el) => ({
-//     [el]: [species.filter((el) => el.location.includes(el))
-//       .map((el) => ({
-//         [el.name]: el.residents.map((el) => el.name),
-//       }))],
-//   }));
-// }
-
-// const animalsNE = species.filter((el) => el.location.includes('NE'))
-//   .map((el) => ({
-//     [el.name]: el.residents.map((el) => el.name),
-//   }));
-
-// const animalsNW = species.filter((el) => el.location.includes('NW'))
-//   .map((el) => ({
-//     [el.name]: el.residents.map((el) => el.name),
-//   }));
-// const animalsSE = species.filter((el) => el.location.includes('SE'))
-//   .map((el) => ({
-//     [el.name]: el.residents.map((el) => el.name),
-//   }));
-// const animalsSW = species.filter((el) => el.location.includes('SW'))
-//   .map((el) => ({
-//     [el.name]: el.residents.map((el) => el.name),
-//   }));
-
-// function test(animal) {
-//   return ['NE', 'NW', 'SE', 'SW'].forEach((el) => {
-//     animal[el].sort();
-//   });
-// }
-
-// console.log(test(animalsNE));
-
-// function getFilterIncludeName(includeNames) {
-//   if (includeNames) {
-//     return {
-//       NE: animalsNE,
-//       NW: animalsNW,
-//       SE: animalsSE,
-//       SW: animalsSW,
-
-//     };
-//   }
-// }
-// console.log(getFilterIncludeName(true));
-
-// function getSorted() {
-//   return {
-//     NE: test(animalsNE),
-//     NW: test(animalsNW),
-//     SE: test(animalsSE),
-//     SW: test(animalsSW),
-//   };
-// }
-
-function getAnimalMap(options) {
-//   if (!options) { return defautlObj; }
-//   const { includeNames, sorted, sex } = options;
-//   if (!includeNames) { return defautlObj; }
-//   if (sorted) {
-//     return getSorted();
-//   }
-//   return getFilterIncludeName(includeNames);
+  return initObj;
 }
 
 module.exports = getAnimalMap;
